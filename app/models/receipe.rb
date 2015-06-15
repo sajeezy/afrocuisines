8056ac5a4a174443232d8594014e867f2f9885ac
Steven Sajja <Stevensajja@gmail.com>
2015-06-15 15:42:47 +0200

    adding thumb

--- app/models/receipe.rb
+++ app/models/receipe.rb
@@ -11,8 +11,7 @@ class Receipe < ActiveRecord::Base
   								   allow_destroy: true
 
   
-	has_attached_file :image, :styles => { :medium => "400x400#>"}
-
+	has_attached_file :image, :styles => { :medium => "400x400#>",:thumb => "100x100>"}
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 	validates :image, presence: true
   validates :title, presence: true
